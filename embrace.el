0221d18ca0a633c14ea3dc8f4948ecfe3c47e1cd
Junpeng Qiu <qjpchmail@gmail.com>
2016-04-28 16:36:01 -0400

    Replace emacs 24.4 requirement with cl-lib

--- embrace.el
+++ embrace.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2016  Junpeng Qiu
 
 ;; Author: Junpeng Qiu <qjpchmail@gmail.com>
-;; Package-Requires: ((emacs "24.4") (expand-region "0.10.0"))
+;; Package-Requires: ((cl-lib "0.5") (expand-region "0.10.0"))
 ;; Keywords: extensions
 
 ;; This program is free software; you can redistribute it and/or modify
